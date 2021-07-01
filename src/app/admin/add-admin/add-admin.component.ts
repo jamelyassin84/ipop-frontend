8d295fc7a2e51aa482808e35d74180df05a02ba1
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-01 21:14:37 +0800

    administrators ui

--- src/app/admin/add-admin/add-admin.component.ts
+++ src/app/admin/add-admin/add-admin.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MunicipalityType } from 'src/app/Types/locations/Municipality.types'
+import { BarangayOfficialType } from 'src/app/Types/officials/BarangayOfficials.types'
 
 @Component({
-  selector: 'app-add-admin',
-  templateUrl: './add-admin.component.html',
-  styleUrls: ['./add-admin.component.scss']
+	selector: 'app-add-admin',
+	templateUrl: './add-admin.component.html',
+	styleUrls: ['./add-admin.component.scss'],
 })
 export class AddAdminComponent implements OnInit {
+	isSubmitting: Boolean = false
+	municipalities: MunicipalityType[] = []
+	barangays: BarangayOfficialType[] = []
+	barangayIsLoading: Boolean = false
+	isLoading: Boolean = false
+	image: any
+	invalidData: any = {}
+	data: any = {}
+	randomImages = [
+		'/assets/ipop/avatars/boy-blue.png',
+		'/assets/ipop/avatars/boyorange.png',
+		'assets/ipop/avatars/girl-black.png',
+		'/assets/ipop/avatars/girl-orange.png',
+	]
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.setRandomImage()
+	}
 
+	setRandomImage() {
+		this.image = this.randomImages[Math.floor(Math.random() * this.randomImages.length)]
+	}
+
+	register() {}
 }
