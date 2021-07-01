110bacf9b299bfc8792fdaf110c078470f3ecf13
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-01 12:19:58 +0800

    in-migration-form

---
+++ src/app/forms/in-migration-form/InMigrationData.ts
@@ -0,0 +1,48 @@
+export const educationalAttainments = [
+	'None Completed',
+	'Elementary Graduate',
+	'High School, not completed',
+	'High School Graduate',
+	'Technical/Vocational',
+	'College, not completed',
+	'College Graduate',
+	'Post Graduate Studies',
+]
+
+export const occupations = ['Gainful', 'Non- Gainful']
+
+export const monthlyIncomes = [
+	'No Income',
+	'Under 5,000',
+	'5,000-9,999',
+	'10,000-14,999',
+	'15,000-19,999',
+	'20,000-24,999',
+	'25,000&over',
+]
+export const placeOfOrigins = [
+	'Other Barangay, within the municipality',
+	'Other Municipality, within the province',
+	'Other Province, within the country',
+	'Abroad',
+]
+export const reasonForInMagratings = [
+	'Transfer to permanent residence(Lot/house purchase)',
+	'Livelihood/Employment',
+	'Marriage',
+	'Live with Parents/ relatives',
+	'Education',
+	'Balik bayan/return to own place',
+	'Land/boundary dispute',
+	'Live in',
+]
+export const specificReasons = [
+	'Transfer to permanent residence(Lot/house purchase)',
+	'Livelihood/Employment',
+	'Marriage',
+	'Live with Parents/ relatives',
+	'Education',
+	'Balik bayan/return to own place',
+	'Land/boundary dispute',
+	'Live in',
+]
