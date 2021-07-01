e6766e3bd4fd1def43c6e6479d485b0743ea058c
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-01 12:49:51 +0800

    marriage

---
+++ src/app/Types/forms/Marriages.types.ts
@@ -0,0 +1,18 @@
+export type MarriagesType = {
+	sorting_number: String
+	municipality: String
+	barangay: String
+	total_cases: String
+	household_number: String
+	case_number: String
+	month: String
+	couple_name: String
+	sex: String
+	age_bracket: String
+	address: String
+	wedding_ceremony_type: String
+	residence_address: String
+	solemnizing_officer: String
+	registered_lcr: String
+	age: String
+}
--- src/app/forms/marriages-form/marriages-form.component.ts
+++ src/app/forms/marriages-form/marriages-form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MarriagesType } from 'src/app/Types/forms/Marriages.types'
+import { BarangayType } from 'src/app/Types/locations/Barangay.types'
+import { MunicipalityType } from 'src/app/Types/locations/Municipality.types'
+import { ageBrackets, months, sexs } from '../birth-form/BirthData'
 
 @Component({
-  selector: 'app-marriages-form',
-  templateUrl: './marriages-form.component.html',
-  styleUrls: ['./marriages-form.component.scss']
+	selector: 'app-marriages-form',
+	templateUrl: './marriages-form.component.html',
+	styleUrls: ['./marriages-form.component.scss'],
 })
 export class MarriagesFormComponent implements OnInit {
+	municipalities: MunicipalityType[] = []
+	barangays: BarangayType[] = []
+	fields: MarriagesType | any = {}
+	barangayIsLoading: Boolean = false
+	isLoading: Boolean = false
+	months = months
+	sexs = sexs
+	ageBrackets = ageBrackets
+	typeOfWeddingCeremonies = ['Church', 'Civil', 'Tribal Rites']
+	solemnizingOfficers = ['Judge', 'Mayor', 'Priest/Pastor/Imam']
+	registeredLCRs = ['Yes', 'No']
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {}
 
+	invalid: any = {}
+
+	save() {}
 }
