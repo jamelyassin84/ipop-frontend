36a0c199bb014ea50c828d42b788b4413d34bb45
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-14 19:16:10 +0800

    population-pyramid form

---
+++ src/app/Types/population/Pyramid.type.ts
@@ -0,0 +1,19 @@
+export type PyramidType = {
+	'Below 1 year old': Number
+	'5-9': Number
+	'10-14': Number
+	'15-19': Number
+	'20-24': Number
+	'25-29': Number
+	'30-34': Number
+	'35-39': Number
+	'40-44': Number
+	'45-49': Number
+	'50-54': Number
+	'55-59': Number
+	'60-64': Number
+	'65-69': Number
+	'70-74': Number
+	'75-79': Number
+	'80 and above': Number
+}
--- src/app/home-components/population/customize-pyramid/customize-pyramid.component.ts
+++ src/app/home-components/population/customize-pyramid/customize-pyramid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { Fire } from 'src/app/components/Alert'
+import { PyramidType } from 'src/app/Types/population/Pyramid.type'
+import { PyramidInputs } from './Pyramid.input'
 
 @Component({
 	selector: 'CustomizePyramid',
@@ -8,5 +11,27 @@ import { Component, OnInit } from '@angular/core'
 export class CustomizePyramidComponent implements OnInit {
 	constructor() {}
 
+	types = ['Provincial', 'Muncipality', 'Barangay']
+
 	ngOnInit(): void {}
+
+	males: PyramidType = PyramidInputs.males
+	females: PyramidType = PyramidInputs.females
+
+	tabs: any = {
+		males: true,
+		famales: false,
+	}
+
+	changeTab(tab: any) {
+		for (let key in this.tabs) {
+			this.tabs[key] = false
+		}
+		this.tabs[tab] = true
+	}
+
+	save() {
+		console.log(this.males)
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+	}
 }
