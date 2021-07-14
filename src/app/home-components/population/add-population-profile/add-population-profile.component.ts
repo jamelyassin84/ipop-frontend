47746f54dba0431c879ad72387bd76cb74d913b5
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-14 18:33:59 +0800

    population profile form

--- src/app/home-components/population/add-population-profile/add-population-profile.component.ts
+++ src/app/home-components/population/add-population-profile/add-population-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Fire } from 'src/app/components/Alert'
 
 @Component({
 	selector: 'AddPopulationProfile',
@@ -8,5 +9,11 @@ import { Component, OnInit } from '@angular/core'
 export class AddPopulationProfileComponent implements OnInit {
 	constructor() {}
 
+	types = ['Provincial', 'Muncipality', 'Barangay']
+
 	ngOnInit(): void {}
+
+	save() {
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+	}
 }
