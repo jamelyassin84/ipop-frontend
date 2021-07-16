e9f9dd6bf7e6a7d85b00596d8b732a747f9f405a
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-16 10:43:41 +0800

    PMOC with charts

---
+++ src/app/home/rpfp/pmoc/PMOCMonthChart.ts
@@ -0,0 +1,28 @@
+export const PMOCMonthChartConfig = {
+	options: {
+		scaleShowVerticalLines: false,
+		responsive: true,
+	},
+	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	legend: true,
+	colors: [],
+	datasets: [
+		{
+			data: [
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+				Math.random(),
+			],
+			label: 'Females',
+		},
+	],
+}
--- src/app/home/rpfp/pmoc/pmoc.component.ts
+++ src/app/home/rpfp/pmoc/pmoc.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ByAgeGroupConfig } from './ByAgeGroup'
+import { ByCIvilStatusConfig } from './ByCIvilStatus'
+import { ByEmploymentStatusConfig } from './ByEmploymentStatus'
+import { ByKnowledgeOnFPConfig } from './ByKnowledgeOnFP'
+import { byMonthlyIncomeConfig } from './byMonthlyIncome'
+import { PMOCMonthChartConfig } from './PMOCMonthChart'
 
 @Component({
-  selector: 'app-pmoc',
-  templateUrl: './pmoc.component.html',
-  styleUrls: ['./pmoc.component.scss']
+	selector: 'app-pmoc',
+	templateUrl: './pmoc.component.html',
+	styleUrls: ['./pmoc.component.scss'],
 })
 export class PmocComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	numberOfCouplesChart = PMOCMonthChartConfig
+	ByAgeGroup = ByAgeGroupConfig
+	ByCIvilStatus = ByCIvilStatusConfig
+	ByEmploymentStatus = ByEmploymentStatusConfig
+	ByKnowledgeOnFP = ByKnowledgeOnFPConfig
+	byMonthlyIncome = byMonthlyIncomeConfig
+	ngOnInit(): void {}
 }
