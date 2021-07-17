1e62f1604729baa353f234211a0be82b44f9f149
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-17 12:22:56 +0800

    technical notes approval

--- src/app/approval-component/others/approval-technical-notes/approval-technical-notes.component.ts
+++ src/app/approval-component/others/approval-technical-notes/approval-technical-notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { TechnicalNotesDummy } from 'src/app/home-components/technical-notes/TechnicalNotesDummy'
 
 @Component({
 	selector: 'Technical-Notes',
@@ -7,6 +8,6 @@ import { Component, OnInit } from '@angular/core'
 })
 export class ApprovalTechnicalNotesComponent implements OnInit {
 	constructor() {}
-
+	TechnicalNotes = TechnicalNotesDummy
 	ngOnInit(): void {}
 }
