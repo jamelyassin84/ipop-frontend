74c3d5dd149d97ce32f645e8c7241610714c6abf
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-17 11:34:04 +0800

    approval of sliders

--- src/app/approval-component/news/approval-slider/approval-slider.component.ts
+++ src/app/approval-component/news/approval-slider/approval-slider.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core'
 	styleUrls: ['./approval-slider.component.scss'],
 })
 export class ApprovalSliderComponent implements OnInit {
+	images = [
+		{ path: '../../../assets/ipop/home/1.jpg' },
+		{ path: '../../../assets/ipop/home/2.jpg' },
+		{ path: '../../../assets/ipop/home/3.jpg' },
+		{ path: '../../../assets/ipop/home/4.jpg' },
+	]
+
 	constructor() {}
 
 	ngOnInit(): void {}
