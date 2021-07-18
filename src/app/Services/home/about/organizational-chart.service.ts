5c41c7961c6ad9b3f05b556eb530484281a6a554
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-18 22:06:07 +0800

    CRUD Organizational Structure

--- src/app/Services/home/about/organizational-chart.service.ts
+++ src/app/Services/home/about/organizational-chart.service.ts
@@ -7,6 +7,6 @@ import { BaseService } from '../../base.service'
 })
 export class OrganizationalChartService extends BaseService {
 	constructor(private _http: HttpClient) {
-		super(_http, '', '')
+		super(_http, 'charts', '')
 	}
 }
--- src/app/home/about/organizational-chart/organizational-chart.component.ts
+++ src/app/home/about/organizational-chart/organizational-chart.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { OrganizationalChartService } from './../../../Services/home/about/organizational-chart.service'
+import { Component, OnInit } from '@angular/core'
+import { Alert } from 'src/app/components/Alert'
+import { ReloadService } from 'src/app/Services/reload.service'
 
 @Component({
-  selector: 'app-organizational-chart',
-  templateUrl: './organizational-chart.component.html',
-  styleUrls: ['./organizational-chart.component.scss']
+	selector: 'app-organizational-chart',
+	templateUrl: './organizational-chart.component.html',
+	styleUrls: ['./organizational-chart.component.scss'],
 })
 export class OrganizationalChartComponent implements OnInit {
+	constructor(private service: OrganizationalChartService, private component: ReloadService) {
+		this.component.shouldReload().subscribe(() => {
+			this.ngOnInit()
+		})
+	}
 
-  constructor() { }
+	ngOnInit(): void {
+		this.service.index().subscribe((data) => {
+			this.img = data.photo.uri
+		})
+	}
 
-  ngOnInit(): void {
-  }
+	img = null
 
+	triggerInputTypeFile() {
+		document.getElementById('input-type-file')?.click()
+	}
+
+	readUrl(event: any) {
+		if (event.target.files && event.target.files[0]) {
+			const reader = new FileReader()
+			reader.readAsDataURL(event.target.files[0])
+			reader.onload = (event) => {
+				this.storeOrganizationalStructure((<FileReader>event.target).result)
+			}
+		}
+	}
+
+	storeOrganizationalStructure(file: any) {
+		let data: any = {}
+		data['photo'] = file
+		this.service.create(data).subscribe((data) => {
+			Alert('Success', 'Oranizational Structure has been changed', 'success')
+		})
+	}
 }
