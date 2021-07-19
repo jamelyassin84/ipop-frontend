00f0d5ec9c3a0dfc4aec456ee00c4a877b653b04
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-19 10:15:25 +0800

    Officials Polished

--- src/app/home-components/population/view-officials/view-officials.component.ts
+++ src/app/home-components/population/view-officials/view-officials.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core'
+import { Deleted, Fire } from 'src/app/components/Alert'
 import { BarangayService } from 'src/app/Services/home/officials/barangay.service'
 import { MunicipalService } from 'src/app/Services/home/officials/municipal.service'
 import { ProvincialService } from 'src/app/Services/home/officials/provincial.service'
 import { SpMemberService } from 'src/app/Services/home/officials/sp-member.service'
 import { LocationService } from 'src/app/Services/locations/province.service'
+import { ReloadService } from 'src/app/Services/reload.service'
 import { MunicipalityType } from 'src/app/Types/locations/Municipality.types'
 import { BarangayOfficialType } from 'src/app/Types/officials/BarangayOfficials.types'
 
@@ -18,8 +20,13 @@ export class ViewOfficialsComponent implements OnInit {
 		private SPMember: SpMemberService,
 		private municipalOfficial: MunicipalService,
 		private barangayOfficial: BarangayService,
-		private location: LocationService
-	) {}
+		private location: LocationService,
+		private component: ReloadService
+	) {
+		this.component.shouldReload().subscribe(() => {
+			this.fetch()
+		})
+	}
 
 	types = ['Provincial Official', 'Sanguniang Panlalawigan Member', 'Municipal Official', 'Barangay Official']
 	type: string = 'Provincial Official'
@@ -44,6 +51,7 @@ export class ViewOfficialsComponent implements OnInit {
 		if (this.type === 'Barangay Official') {
 			this.viewBarangayOfficial()
 		}
+		this.currentOfficial = this.type
 	}
 
 	data: any = {}
@@ -69,6 +77,48 @@ export class ViewOfficialsComponent implements OnInit {
 	viewBarangayOfficial() {
 		this.barangayOfficial.index(`barangay=${this.data.barangay}`).subscribe((officials: any) => {
 			this.officials = officials
+		})
+	}
+
+	currentOfficial: any = {}
+	removeOfficial(id: number) {
+		Fire('Remove Official?', 'Are you sure you want to remove this data?', 'info', () => {
+			if (this.type === 'Provincial Official') {
+				this.removeProvincialOfficial(id)
+			}
+			if (this.type === 'Sanguniang Panlalawigan Member') {
+				this.removeSPMember(id)
+			}
+			if (this.type === 'Municipal Official') {
+				this.removeMunicipalOfficial(id)
+			}
+			if (this.type === 'Barangay Official') {
+				this.removeBarangayOfficial(id)
+			}
+		})
+	}
+
+	removeProvincialOfficial(id: number) {
+		this.provincialOfficial.destroy(id).subscribe(() => {
+			Deleted()
+		})
+	}
+
+	removeSPMember(id: number) {
+		this.SPMember.destroy(id).subscribe(() => {
+			Deleted()
+		})
+	}
+
+	removeMunicipalOfficial(id: number) {
+		this.municipalOfficial.destroy(id).subscribe(() => {
+			Deleted()
+		})
+	}
+
+	removeBarangayOfficial(id: number) {
+		this.barangayOfficial.destroy(id).subscribe(() => {
+			Deleted()
 		})
 	}
 
