7e4420e6d12fb0c1639fbffe5174e1c615e6840e
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-19 22:15:47 +0800

    Migration Add

--- src/app/home-components/demographic/migrations/add-migration-data/add-migration-data.component.ts
+++ src/app/home-components/demographic/migrations/add-migration-data/add-migration-data.component.ts
@@ -1,5 +1,6 @@
+import { LocalMigrationDataService } from './../../../../Services/home/demographic/migrations/local-migration-data.service'
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
 
 @Component({
 	selector: 'AddMigrationData',
@@ -7,13 +8,28 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./add-migration-data.component.scss'],
 })
 export class AddMigrationDataComponent implements OnInit {
-	constructor() {}
-
-	types = ['Provincial', 'Muncipality', 'Barangay']
+	constructor(private service: LocalMigrationDataService) {}
 
 	ngOnInit(): void {}
 
+	data: any = {
+		barangay: null,
+		municipality: null,
+		year: null,
+		gender: 'male',
+	}
+
+	fetch(event: any) {
+		this.data.barangay = event.barangay
+		this.data.municipality = event.municipality
+		this.data.year = event.year
+	}
+
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
