f4db2c0230a5997274fde553c04fdf22171562dd
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-19 21:02:15 +0800

    Death Data add

--- src/app/home-components/demographic/deaths/add-death-data/add-death-data.component.ts
+++ src/app/home-components/demographic/deaths/add-death-data/add-death-data.component.ts
@@ -1,5 +1,6 @@
+import { LocalDeathDataService } from './../../../../Services/home/demographic/deaths/local-death-data.service'
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
 
 @Component({
 	selector: 'AddDeathData',
@@ -7,13 +8,28 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./add-death-data.component.scss'],
 })
 export class AddDeathDataComponent implements OnInit {
-	constructor() {}
-
-	types = ['Provincial', 'Muncipality', 'Barangay']
+	constructor(private service: LocalDeathDataService) {}
 
 	ngOnInit(): void {}
 
+	data: any = {
+		barangay: null,
+		municipality: null,
+		year: null,
+		gender: 'male',
+	}
+
+	fetch(event: any) {
+		this.data.barangay = event.barangay
+		this.data.municipality = event.municipality
+		this.data.year = event.year
+	}
+
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
