94473031780eac6a959a9977cc8f07b70751c818
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-19 23:13:10 +0800

    Added PMOC Data

--- src/app/home-components/rpfp/pmoc/add-pmoc-data/add-pmoc-data.component.ts
+++ src/app/home-components/rpfp/pmoc/add-pmoc-data/add-pmoc-data.component.ts
@@ -1,5 +1,6 @@
+import { PmocDataService } from './../../../../Services/home/rpfp/pmoc/pmoc-data.service'
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
 
 @Component({
 	selector: 'AddPMOCData',
@@ -7,12 +8,28 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./add-pmoc-data.component.scss'],
 })
 export class AddPmocDataComponent implements OnInit {
-	constructor() {}
-	types = ['Provincial', 'Muncipality', 'Barangay']
+	constructor(private service: PmocDataService) {}
 
 	ngOnInit(): void {}
 
+	data: any = {
+		barangay: null,
+		municipality: null,
+		year: null,
+		gender: 'male',
+	}
+
+	fetch(event: any) {
+		this.data.barangay = event.barangay
+		this.data.municipality = event.municipality
+		this.data.year = event.year
+	}
+
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
