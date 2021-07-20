3b6d3a41d263efa592bd651a981d79ad58627364
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-20 19:29:01 +0800

    Personnel Directory

--- src/app/home-components/about/add-personnel-directory/add-personnel-directory.component.ts
+++ src/app/home-components/about/add-personnel-directory/add-personnel-directory.component.ts
@@ -1,5 +1,6 @@
+import { PersonnelDirectoryService } from './../../../Services/home/about/personnel-directory.service'
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
 import { PersonnelTypes } from 'src/app/home/about/personnel-directory/PersonnelTypes'
 
 @Component({
@@ -8,13 +9,17 @@ import { PersonnelTypes } from 'src/app/home/about/personnel-directory/Personnel
 	styleUrls: ['./add-personnel-directory.component.scss'],
 })
 export class AddPersonnelDirectoryComponent implements OnInit {
-	constructor() {}
+	constructor(private service: PersonnelDirectoryService) {}
 
 	types = PersonnelTypes
 
 	ngOnInit(): void {}
-
+	data: any = {}
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
--- src/app/home/about/personnel-directory/personnel-directory.component.ts
+++ src/app/home/about/personnel-directory/personnel-directory.component.ts
@@ -1,4 +1,7 @@
+import { Deleted, Fire } from 'src/app/components/Alert'
 import { Component, OnInit } from '@angular/core'
+import { PersonnelDirectoryService } from 'src/app/Services/home/about/personnel-directory.service'
+import { ReloadService } from 'src/app/Services/reload.service'
 import { PersonnelTypes } from './PersonnelTypes'
 
 @Component({
@@ -7,9 +10,53 @@ import { PersonnelTypes } from './PersonnelTypes'
 	styleUrls: ['./personnel-directory.component.scss'],
 })
 export class PersonnelDirectoryComponent implements OnInit {
-	constructor() {}
+	constructor(private service: PersonnelDirectoryService, private component: ReloadService) {
+		this.component.shouldReload().subscribe(() => {
+			this.ngOnInit()
+		})
+	}
 
 	types = PersonnelTypes
 
-	ngOnInit(): void {}
+	ppo: any = []
+	ad: any = []
+	trd: any = []
+	fod1: any = []
+	fod2: any = []
+	fod3: any = []
+	fod4: any = []
+	fod5: any = []
+	apvw: any = []
+	bod: any = []
+	bspo1: any = []
+	bspo2: any = []
+	bspo3: any = []
+	bspo4: any = []
+	bspo5: any = []
+	ngOnInit(): void {
+		this.service.index().subscribe((data) => {
+			this.ppo = data.ppo
+			this.ad = data.ad
+			this.trd = data.trd
+			this.fod1 = data.fod1
+			this.fod2 = data.fod2
+			this.fod3 = data.fod3
+			this.fod4 = data.fod4
+			this.fod5 = data.fod5
+			this.apvw = data.apvw
+			this.bod = data.bod
+			this.bspo1 = data.bspo1
+			this.bspo2 = data.bspo2
+			this.bspo3 = data.bspo3
+			this.bspo4 = data.bspo4
+			this.bspo5 = data.bspo5
+		})
+	}
+	remove(id: number) {
+		Fire('Remove Personnel?', 'Are you sure you want to permanently remove this data?', 'info', () => {
+			this.service.destroy(id).subscribe(() => {
+				Deleted()
+			})
+		})
+	}
 }
