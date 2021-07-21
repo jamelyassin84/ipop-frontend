47e26c78076d1d805c0b400cb6dc5219afdcf038
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-21 11:52:38 +0800

    Fixed Teen Center UI

--- src/app/home/ahyd/teen-centers/teen-centers.component.ts
+++ src/app/home/ahyd/teen-centers/teen-centers.component.ts
@@ -1,6 +1,8 @@
+import { BaseService } from 'src/app/Services/base.service'
+import { TeenCenterDataService } from './../../../Services/home/ahyd/teen-center/teen-center-data.service'
 import { Component, OnInit } from '@angular/core'
 import { Subscription } from 'rxjs'
-import { Fire } from 'src/app/components/Alert'
+import { Deleted, Fire } from 'src/app/components/Alert'
 import { ReloadService } from 'src/app/Services/reload.service'
 
 @Component({
@@ -9,10 +11,10 @@ import { ReloadService } from 'src/app/Services/reload.service'
 	styleUrls: ['./teen-centers.component.scss'],
 })
 export class TeenCentersComponent implements OnInit {
-	constructor(private component: ReloadService) {
+	constructor(private component: ReloadService, private service: TeenCenterDataService) {
 		this.subscriptions.add(
 			this.component.shouldReload().subscribe(() => {
-				this.ngOnInit()
+				this.setTab(this.district)
 			})
 		)
 	}
@@ -23,9 +25,57 @@ export class TeenCentersComponent implements OnInit {
 		this.subscriptions.unsubscribe()
 	}
 
-	ngOnInit(): void {}
+	tabs: any = {
+		district1: false,
+		district2: false,
+		district3: false,
+		district4: false,
+		district5: false,
+	}
 
-	remove() {
-		Fire('Remove Data?', 'Are you sure you want to remove this data?', 'info', () => {})
+	district = 'district1'
+
+	setTab(district: string) {
+		for (let key in this.tabs) {
+			this.tabs[key] = false
+		}
+		this.district = district
+		this.tabs[district] = true
+		this.getTeenCenters()
+	}
+
+	ngOnInit(): void {
+		this.setTab('district1')
+	}
+
+	teenCenters: any = []
+	getTeenCenters() {
+		new BaseService(this.service.http, this.service.url, `district=${this.transformDistrict(this.district)}`).index().subscribe((data: any) => {
+			this.teenCenters = data
+		})
+	}
+
+	transformDistrict(district: string) {
+		if (district === 'district1') {
+			return 'I'
+		}
+		if (district === 'district2') {
+			return 'II'
+		}
+		if (district === 'district3') {
+			return 'III'
+		}
+		if (district === 'district4') {
+			return 'IV'
+		}
+		return 'V'
+	}
+
+	remove(id: number) {
+		Fire('Remove Data?', 'Are you sure you want to remove this data?', 'info', () => {
+			this.service.destroy(id).subscribe(() => {
+				Deleted()
+			})
+		})
 	}
 }
