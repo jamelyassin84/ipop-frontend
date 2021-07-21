ad411f45422c780a535f116e2ba109b49fc3354d
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-21 12:08:59 +0800

    remove personnel incharge

--- src/app/home-components/ahyd/teen-centers/view-tc-personnel-incharge/view-tc-personnel-incharge.component.ts
+++ src/app/home-components/ahyd/teen-centers/view-tc-personnel-incharge/view-tc-personnel-incharge.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Subscription } from 'rxjs'
+import { Deleted, Fire } from 'src/app/components/Alert'
+import { BaseService } from 'src/app/Services/base.service'
+import { PersonnelInchargeOfTeenCenterService } from 'src/app/Services/home/ahyd/teen-center/personnel-incharge-of-teen-center.service'
+import { ReloadService } from 'src/app/Services/reload.service'
 
 @Component({
 	selector: 'ViewPersonnelIncharge',
@@ -7,11 +11,37 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./view-tc-personnel-incharge.component.scss'],
 })
 export class ViewTcPersonnelInchargeComponent implements OnInit {
-	constructor() {}
+	constructor(private service: PersonnelInchargeOfTeenCenterService, private component: ReloadService) {
+		this.subscriptions.add(
+			this.component.shouldReload().subscribe(() => {
+				this.ngOnInit()
+			})
+		)
+	}
 
-	ngOnInit(): void {}
+	private subscriptions = new Subscription()
+
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe()
+	}
+
+	personnels: any = []
+
 	@Input() teen_center_id: number = 0
-	remove() {
-		Fire('Remove Data?', 'Are you sure you want to remove this data?', 'info', () => {})
+
+	ngOnInit(): void {
+		setTimeout(() => {
+			new BaseService(this.service.http, this.service.url, `sbmptc_id=${this.teen_center_id}`).index().subscribe((data: any) => {
+				this.personnels = data
+			})
+		}, 300)
+	}
+
+	remove(id: number) {
+		Fire('Remove Data?', 'Are you sure you want to remove this data?', 'info', () => {
+			this.service.destroy(id).subscribe(() => {
+				Deleted()
+			})
+		})
 	}
 }
