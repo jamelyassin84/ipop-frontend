6a02d04a18ec19151b45f8346b820dae59b4123a
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-21 11:04:41 +0800

    Added Teen Center Global Data

--- src/app/home-components/ahyd/teen-centers/add-focal-ahyd-team/add-focal-ahyd-team.component.ts
+++ src/app/home-components/ahyd/teen-centers/add-focal-ahyd-team/add-focal-ahyd-team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
+import { AhydTeamService } from 'src/app/Services/home/ahyd/teen-center/ahyd-team.service'
 
 @Component({
 	selector: 'AddAHYDTeam',
@@ -7,11 +8,30 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./add-focal-ahyd-team.component.scss'],
 })
 export class AddFocalAhydTeamComponent implements OnInit {
-	constructor() {}
+	constructor(private service: AhydTeamService) {}
 
 	ngOnInit(): void {}
 
+	data: any = {}
+
+	image: any = '../../../../../assets/avatars/face-7.jpg'
+
+	readURL(event: any) {
+		if (event.target.files && event.target.files[0]) {
+			const reader = new FileReader()
+			reader.readAsDataURL(event.target.files[0])
+			reader.onload = (event: any) => {
+				this.data.photo = event.target.result
+				this.image = event.target.result
+			}
+		}
+	}
+
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
--- src/app/home-components/ahyd/teen-centers/add-focal-person/add-focal-person.component.ts
+++ src/app/home-components/ahyd/teen-centers/add-focal-person/add-focal-person.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
-import { Fire } from 'src/app/components/Alert'
+import { Created, Fire } from 'src/app/components/Alert'
+import { FocalPersonsService } from 'src/app/Services/home/ahyd/teen-center/focal-persons.service'
 
 @Component({
 	selector: 'AddFocalPersons',
@@ -7,11 +8,30 @@ import { Fire } from 'src/app/components/Alert'
 	styleUrls: ['./add-focal-person.component.scss'],
 })
 export class AddFocalPersonComponent implements OnInit {
-	constructor() {}
+	constructor(private service: FocalPersonsService) {}
 
 	ngOnInit(): void {}
 
+	data: any = {}
+
+	image: any = '../../../../../assets/avatars/face-7.jpg'
+
+	readURL(event: any) {
+		if (event.target.files && event.target.files[0]) {
+			const reader = new FileReader()
+			reader.readAsDataURL(event.target.files[0])
+			reader.onload = (event: any) => {
+				this.data.photo = event.target.result
+				this.image = event.target.result
+			}
+		}
+	}
+
 	save() {
-		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {})
+		Fire('Save Changes?', 'Are you sure you want to add this data?', 'info', () => {
+			this.service.create(this.data).subscribe(() => {
+				Created()
+			})
+		})
 	}
 }
