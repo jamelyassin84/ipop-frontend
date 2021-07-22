8fa0ed1882a5d1761e1047fb9a0eba1bb145151f
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-22 19:17:14 +0800

    approve and disapprove

--- src/app/approvals/approvals.component.ts
+++ src/app/approvals/approvals.component.ts
@@ -1,3 +1,4 @@
+import { Fire } from 'src/app/components/Alert'
 import { UserType } from 'src/app/Types/User.types'
 import { Component, OnInit } from '@angular/core'
 import { Alert } from '../components/Alert'
@@ -9,17 +10,39 @@ import { ApprovalsService } from '../Services/Independent/approvals.service'
 	styleUrls: ['./approvals.component.scss'],
 })
 export class ApprovalsComponent implements OnInit {
-	constructor(private service: ApprovalsService) {}
+	constructor(private service: ApprovalsService) {
+		setInterval(() => {
+			localStorage.setItem('hideNav', 'true')
+		}, 50)
+	}
 
 	approvals: Approval[] = []
 	ngOnInit(): void {
+		this.approvals = []
 		this.service.index().subscribe((data: any) => {
 			this.approvals = data.data
 		})
 	}
 
-	alert1() {
-		Alert('aw', '', 'success')
+	approve(mode: boolean) {
+		const approval = mode === true ? 'Approve' : 'Discard'
+		if (mode === true) {
+			Fire(`${approval} content?`, `Are you sure you want to ${approval} this content?`, 'info', () => {
+				this.service.update(this.data.id, { approved: mode }).subscribe(() => {
+					Alert('Horay!', 'Content has beeen succesfully posted', 'success')
+					this.ngOnInit()
+					this.type = ''
+				})
+			})
+		} else {
+			Fire(`${approval} content?`, `Are you sure you want to ${approval} this content?`, 'info', () => {
+				this.service.destroy(this.data.id).subscribe(() => {
+					Alert('Content Discarded', 'Content has beeen  discarded', 'warning')
+					this.ngOnInit()
+					this.type = ''
+				})
+			})
+		}
 	}
 
 	type: string = ''
