1f823f6f36e932bae2c4413aeddcf7f85c4d140d
Jamel Yassin <jamelyassin84@gmail.com>
2021-07-23 16:09:01 +0800

    dynamic record path

--- src/app/records/uploaded-records/uploaded-records.component.ts
+++ src/app/records/uploaded-records/uploaded-records.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Alert } from 'src/app/components/Alert'
 import { BaseService } from 'src/app/Services/base.service'
 import { FileUploadsService } from 'src/app/Services/home/others/file-uploads.service'
+import { recordNavs } from '../RecordNav'
 
 @Component({
 	selector: 'app-uploaded-records',
@@ -11,13 +12,25 @@ import { FileUploadsService } from 'src/app/Services/home/others/file-uploads.se
 export class UploadedRecordsComponent implements OnInit {
 	constructor(private service: FileUploadsService) {}
 
+	navs = recordNavs
+
 	ngOnInit(): void {
+		this.setType('CPDB')
+	}
+
+	type = 'CPDB'
+	setType(type: string) {
+		this.type = type
 		this.index()
 	}
 
 	files: any = []
 	index() {
-		new BaseService(this.service.http, this.service.url, 'type=Birth')
+		new BaseService(
+			this.service.http,
+			this.service.url,
+			`type=${this.type}`
+		)
 			.index()
 			.subscribe((data) => {
 				this.files = data
