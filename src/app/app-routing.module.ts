82f174f6b0362dc23cf67d2f7ecea8987449eb8c
Jamel Yassin <jamelyassin84@gmail.com>
2022-05-18 20:40:59 +0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router'
 
 import { LoginComponent } from './pages/login/login.component'
 import { HomeComponent } from './pages/home/home.component'
@@ -263,7 +263,13 @@ const routes: Routes = [
 ]
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports: [
+		RouterModule.forRoot(routes, {
+			useHash: true,
+			preloadingStrategy: PreloadAllModules,
+			scrollPositionRestoration: 'top',
+		}),
+	],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
