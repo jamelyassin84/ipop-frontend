0d678fb2ab3b3dcf94b68d542dc65045fd652ff3
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-03 18:18:02 +0400

    negative values

--- src/app/home/demographic/migrations-demographic/migrations-demographic.component.ts
+++ src/app/home/demographic/migrations-demographic/migrations-demographic.component.ts
@@ -138,6 +138,8 @@ export class MigrationsDemographicComponent implements OnInit {
 
 				let minimum: number[] = []
 
+				console.log(data)
+
 				for (let index of data) {
 					for (let key in index) {
 						if (index[key] < 0) {
