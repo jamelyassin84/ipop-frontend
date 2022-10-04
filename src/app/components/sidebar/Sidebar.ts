c24821c6bacd2b14dcd656decdda292b6b23b0b0
Jamel Yassin <jamelyassin84@gmail.com>
2022-10-04 20:45:22 +0400

    Changes

--- src/app/components/sidebar/Sidebar.ts
+++ src/app/components/sidebar/Sidebar.ts
@@ -3,13 +3,13 @@ export const normal = [
         name: 'Home',
         route: 'index/news',
         color: 'c-blue-700',
-        icon: 'ti-home',
+        icon: 'bi bi-house',
     },
     {
         name: 'Bulk Records',
         route: 'bulk-records/upload',
         color: 'c-green-600',
-        icon: 'ti-harddrives',
+        icon: 'bi bi-folder-fill',
     },
 ]
 
@@ -18,30 +18,30 @@ export const ADMIN = [
         name: 'Home',
         route: 'index/news',
         color: 'c-blue-900',
-        icon: 'ti-home',
+        icon: 'bi bi-house-fill',
     },
     {
         name: 'Approvals',
         route: 'approvals',
         color: 'c-red-500',
-        icon: 'ti-slice',
+        icon: 'bi bi-pen-fill',
     },
     {
         name: 'Bulk Records',
         route: 'bulk-records/upload',
         color: 'c-light-green-500',
-        icon: 'ti-harddrives',
+        icon: 'bi bi-folder-fill',
     },
     {
-        name: 'Admins',
+        name: 'Administrators',
         route: 'admins/view',
         color: 'c-orange-500',
-        icon: 'ti-user',
+        icon: 'bi bi-person-badge-fill',
     },
     {
         name: 'Logs',
         route: 'logs',
         color: 'c-orange-900',
-        icon: 'ti-layers-alt',
+        icon: 'bi bi-hdd-stack-fill',
     },
 ]
