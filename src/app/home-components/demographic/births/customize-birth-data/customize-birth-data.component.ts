7de9cbfa337e8200f31be9c16dfc300ede3f0af8
Jamel Eid Yassin <jamelyassin84@gmail.com>
2023-08-14 20:40:29 +0400

    Changes

--- src/app/home-components/demographic/births/customize-birth-data/customize-birth-data.component.ts
+++ src/app/home-components/demographic/births/customize-birth-data/customize-birth-data.component.ts
@@ -1,6 +1,12 @@
+import {map, take} from 'rxjs/operators'
+import {Store, select} from '@ngrx/store'
 import {LocalBirthDataService} from './../../../../Services/home/demographic/births/local-birth-data.service'
 import {Component, Input, OnInit} from '@angular/core'
+import {AppState} from 'src/app/app-core/store/core/app.state'
+import {LocationFIlter} from 'src/app/app-core/store/ngrx/locatition-filter/location-filter.model'
 import {Fire, HasApprovals, Updated} from 'src/app/modules/extras/Alert'
+import {StateEnum} from 'src/app/app-core/store/core/state.enum'
+import {TransformEntity} from 'src/@digital_brand_work/helpers/entity.helper'
 
 @Component({
     selector: 'CustomizeBirthData',
@@ -8,35 +14,62 @@ import {Fire, HasApprovals, Updated} from 'src/app/modules/extras/Alert'
     styleUrls: ['./customize-birth-data.component.scss'],
 })
 export class CustomizeBirthDataComponent implements OnInit {
-    constructor(private service: LocalBirthDataService) {}
+    constructor(
+        private _store: Store<AppState>,
+        private service: LocalBirthDataService,
+    ) {}
 
-    @Input()
-    data: any = {}
+    @Input('data')
+    set setData(data: any) {
+        console.log(data)
+        this.data = {...this.data, ...data}
+    }
+
+    readonly location$ = this._store.pipe(
+        select(StateEnum.LOCATION_FILTERS),
+        map((x) => new TransformEntity(x).toObject()),
+    )
+
+    data: any = {
+        barangay: null,
+        municipality: null,
+        year: null,
+        gender: 'male',
+    }
 
     isLoading: boolean = false
 
     ngOnInit(): void {}
 
     save() {
-        const data = {...this.data}
+        this.location$.pipe(take(1)).subscribe((location) => {
+            const data = {...this.data, ...location}
 
-        for (let key in data) {
-            if (data[key] !== null) {
-                data[key] = data[key] + ''
+            for (let key in data) {
+                if (data[key] !== null) {
+                    data[key] = data[key] + ''
+                }
             }
-        }
-        Fire(
-            'Save Changes?',
-            'Are you sure you want to add this data?',
-            'info',
-            () => {
-                this.isLoading = true
+            Fire(
+                'Save Changes?',
+                'Are you sure you want to add this data?',
+                'info',
+                () => {
+                    this.isLoading = true
 
-                this.service.update(data.id, data).subscribe(() => {
-                    HasApprovals('Updated')
-                    this.isLoading = false
-                })
-            },
-        )
+                    if (!data.id) {
+                        this.service.create(data).subscribe(() => {
+                            HasApprovals('Added')
+                            this.isLoading = false
+                        })
+                    }
+
+                    this.service.update(data.id, data).subscribe(() => {
+                        HasApprovals('Updated')
+                        this.isLoading = false
+                    })
+                },
+            )
+        })
     }
 }
--- src/app/modules/extras/Alert.ts
+++ src/app/modules/extras/Alert.ts
@@ -1,80 +1,80 @@
 import Swal from 'sweetalert2'
 
 export function Fire(
-	title: string,
-	text: string,
-	type: any,
-	callback: Function
+    title: string,
+    text: string,
+    type: any,
+    callback: Function,
 ) {
-	Swal.fire({
-		title: title,
-		text: text,
-		icon: type,
-		showCancelButton: true,
-		confirmButtonText: 'Yes',
-		cancelButtonText: 'Cancel',
-	}).then((result: any) => {
-		if (result.value) {
-			return callback()
-		}
-	})
-	if (type == 'error') {
-		type = 'warning'
-	}
-	const audio = new Audio(`/assets/ipop/audio/${type}.mp3`)
-	audio.volume = 0.5
-	audio.play()
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: type,
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'Cancel',
+    }).then((result: any) => {
+        if (result.value) {
+            return callback()
+        }
+    })
+    if (type == 'error') {
+        type = 'warning'
+    }
+    // const audio = new Audio(`/assets/ipop/audio/${type}.mp3`)
+    // audio.volume = 0.5
+    // audio.play()
 }
 
 export function Alert(title: string, text: string, type: any) {
-	Swal.fire(title, text, type)
-	if (type == 'error') {
-		type = 'warning'
-	}
-	const audio = new Audio(`/assets/ipop/audio/${type}.mp3`)
-	audio.volume = 0.5
-	audio.play()
+    Swal.fire(title, text, type)
+    if (type == 'error') {
+        type = 'warning'
+    }
+    // const audio = new Audio(`/assets/ipop/audio/${type}.mp3`)
+    // audio.volume = 0.5
+    // audio.play()
 }
 
 export function ValidationError() {
-	Alert('Error', 'One or more fields should not be empty', 'error')
+    Alert('Error', 'One or more fields should not be empty', 'error')
 }
 
 export function Created() {
-	Alert('Success', 'Data has been Successfully Created', 'success')
+    Alert('Success', 'Data has been Successfully Created', 'success')
 }
 
 export function Updated() {
-	Alert('Success', 'Data has been Successfully Updated', 'success')
+    Alert('Success', 'Data has been Successfully Updated', 'success')
 }
 
 export function Deleted() {
-	Alert('Success', 'Data has been Successfully Deleted', 'success')
+    Alert('Success', 'Data has been Successfully Deleted', 'success')
 }
 
 export function HasApprovals(mode: string) {
-	if (localStorage.getItem('role') !== 'Super Admin') {
-		return Alert(
-			'Your content has been sent to admin',
-			'Please wait for your content to be approved by the administrator',
-			'success'
-		)
-	}
-	if (mode === 'Created') {
-		return Created()
-	}
-	if (mode === 'Updated') {
-		return Updated()
-	}
-	return Deleted()
+    if (localStorage.getItem('role') !== 'Super Admin') {
+        return Alert(
+            'Your content has been sent to admin',
+            'Please wait for your content to be approved by the administrator',
+            'success',
+        )
+    }
+    if (mode === 'Created') {
+        return Created()
+    }
+    if (mode === 'Updated') {
+        return Updated()
+    }
+    return Deleted()
 }
 
 export function Welcome(name: String) {
-	Alert('Success', `Welcome Back! ${name}`, 'success')
+    Alert('Success', `Welcome Back! ${name}`, 'success')
 }
 
 export function pop() {
-	const audio = new Audio(`/assets/ipop/audio/pop1.mp3`)
-	audio.volume = 0.1
-	audio.play()
+    // const audio = new Audio(`/assets/ipop/audio/pop1.mp3`)
+    // audio.volume = 0.1
+    // audio.play()
 }
